f8d8817d27bdccdb847dec43725055e998951dc0
Rory Fahy <685@holbertonschool.com>
2019-02-07 03:37:22 +0000

    added 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this functions uses the printf command
+ *
+ * Return: 0 indicates succesful run
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
