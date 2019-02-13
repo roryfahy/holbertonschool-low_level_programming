054ff1bfde11f190e08662998404de5c3f7d419d
Rory Fahy <685@holbertonschool.com>
2019-02-13 02:19:51 +0000

    10 attempt

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * add - adds two intgers
+ * @a: holds first int value
+ * @b: holds second int value
+ *
+ * Return: sum of two integers
+ */
+int add(int a, int b)
+{
+	int x;
+
+	x = a + b;
+	return (x);
+}
