1d8f81071c189cd03b7db9cda367432d73298ce7
Rory Fahy <685@holbertonschool.com>
2019-02-14 21:41:30 +0000

    attempt at 6

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_line - draws a straight line of n length
+ * @n: stores int value
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
