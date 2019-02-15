57f6bcce57ac9db721a62cc4a9693679e7b6dbe7
Rory Fahy <685@holbertonschool.com>
2019-02-15 03:22:56 +0000

    save for the night

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_triangle - makes # triangle of size size
+ * @size: holds integer of size of square
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				if (j > (1 / i))
+					_putchar(' ');
+				else
+					_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
