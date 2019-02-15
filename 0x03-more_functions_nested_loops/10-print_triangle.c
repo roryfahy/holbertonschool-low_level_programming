57af85757b9617e4bc4af4d8b9c9a0cb51d7f6bb
Rory <685@holbertonschool.com>
2019-02-15 05:29:31 +0000

    sleep answered 10

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -14,7 +14,7 @@ void print_triangle(int size)
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
-				if (j > (1 / i))
+				if (j < (size - i))
 					_putchar(' ');
 				else
 					_putchar('#');
