4e1b3b6143e3fb276a8420cfa7f72ab4a322f947
Rory <685@holbertonschool.com>
2019-02-24 20:53:48 +0000

    switching computers

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -8,10 +8,19 @@
 char *cap_string(char *zard)
 {
 	char *pika = zard;
+	char n[] = " \n\t,;.!?\"(){}";
+	char *sep;
 
+	if (*pika >= 'a' && *pika <= 'z')
+		*pika = (*pika) - 32;
 	for (; *pika; pika++)
 	{
-		if (*pika == '
-
-			return (zard);
+		for (sep = n; *sep; sep++)
+			if (*pika == *sep && *(pika + 1) <= 'z' && *(pika + 1) >= 'a')
+			{
+				*(pika + 1) = (*pika + 1) - 32;
+				break;
+			}
+	}
+	return (zard);
 }
