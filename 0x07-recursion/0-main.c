20f701011d74439d4bd60beea58c333bd2b95233
Rory Fahy <685@holbertonschool.com>
2019-02-27 21:34:21 +0000

    attempted 0

---
+++ 0x07-recursion/0-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_puts_recursion("Betty Holberton");
+	fflush(stdout);
+	return (0);
+}
---
+++ 0x07-recursion/README.md
@@ -0,0 +1,3 @@
+this is the readme file for project 0x07 of the low level programming track
+
+the mandatory exercises are 0 - 7 and will be described below as I go along
