237ffb83741fb685e0ee0da91fb85dc2b9ab15ae
Rory Fahy <685@holbertonschool.com>
2019-03-05 19:27:15 +0000

    update

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * create_array - create array of chars and initialze it with a specific char
+ * @size: size of array to print?
+ * @c: char to initialize array to
+ *
+ * Return: a pointer to the first address of the char array
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (
+}
---
+++ 0x0A-malloc_free/README.md
@@ -0,0 +1,4 @@
+# 0x0A malloc and free
+
+0x0A is the directory for the holberton project on learning dynamic memory
+allocation using the commands malloc and free
