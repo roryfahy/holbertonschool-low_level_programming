ca951c18987af0124ea3a455e1e2a817fe614c13
Rory Fahy <685@holbertonschool.com>
2019-03-13 20:47:09 +0000

    attempt problems 1 and 2

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - search for an integer in an array
+ * @array: integer array containing the operand items
+ * @size: size of array
+ * @cmp: pointer to function taking an int and returning an int
+ *
+ * Return: index of first matching item in the array or -1 on failure
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+	for (; size > 0; size--, i++)
+		if (cmp(array[i]))
+			return (i);
+	return (-1);
+}
