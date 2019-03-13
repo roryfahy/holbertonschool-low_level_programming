58f5859d7d41c62d9d7f559f2ef097098a12a81d
Rory Fahy <685@holbertonschool.com>
2019-03-13 20:49:25 +0000

    added edge cases for null in array or cmp

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -12,6 +12,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
+	if (cmp == NULL || array == NULL)
+		return (-1);
 	if (size <= 0)
 		return (-1);
 	for (; size > 0; size--, i++)
