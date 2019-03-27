ecf08ad153fd309a71a736778dd9c2df7d67c389
rmf10003 <685@holbertonschool.com>
2019-03-27 01:13:15 +0000

    fix that is for free points

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -8,14 +8,19 @@
  */
 char *rot13(char *ptr)
 {
+	int i, j;
 	char reg[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char rot[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
+	i = j = 0;
 	if (!ptr)
 		return (NULL);
 	for (; ptr[i]; i++)
 		for (j = 0; reg[j]; j++)
 			if (ptr[i] == reg[j])
+			{
 				ptr[i] = rot[j];
+				break;
+			}
 	return (ptr);
 }
