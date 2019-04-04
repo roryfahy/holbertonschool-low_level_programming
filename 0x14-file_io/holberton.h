cd406601e3ac2d105b7597661bbbb27b1a1aa343
rmf10003 <685@holbertonschool.com>
2019-04-04 02:40:34 +0000

    type cast for 0

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -1,6 +1,7 @@
 #ifndef _H_
 #define _H_
 
+ssize_t _readwrite(char *buffer, int fd);
 void _rw(char *buffer, int from, int to, char *src, char *dest);
 ssize_t safe_write(int fd, char *buff, size_t buff_size);
 ssize_t read_textfile(const char *filename, size_t letters);
