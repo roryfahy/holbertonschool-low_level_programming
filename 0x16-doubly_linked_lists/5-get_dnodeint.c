39c166f964fe9646018d7c95393fb063c63e3baf
Rory <685@holbertonschool.com>
2019-05-03 15:38:04 +0000

    attempt 5 and 6

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -1,11 +1,21 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index -
- *
- * Return: 
+ * get_dnodeint_at_index - return nth node
+ * @head: pointer to dll
+ * @index: index of node
+ * Return: node at index or NULL if not found
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-
+	if (!head)
+		return (NULL);
+	while (index > 0 && head)
+	{
+		index--;
+		head = head->next;
+	}
+	if (index > 0)
+		return (NULL);
+	return (head);
 }
--- 0x16-doubly_linked_lists/6-sum_dlistint.c
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
 /**
- * sum_dlistint -
- *
- * Return: 
+ * sum_dlistint - sum a dll
+ * @head: pointer to dll
+ * Return: sum of elements in list
  */
 int sum_dlistint(dlistint_t *head)
 {
+	int sum = 0;
 
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
