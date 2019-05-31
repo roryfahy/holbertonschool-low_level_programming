859054fd6942200d1c68ad79b3f86ecd4b2bcf1b
rmf10003 <685@holbertonschool.com>
2019-05-31 17:41:47 +0000

    initial commit

--- 0x16-doubly_linked_lists/8-main.c
+++ 0x16-doubly_linked_lists/8-main.c
@@ -13,17 +13,10 @@ int main(void)
     dlistint_t *head;
 
     head = NULL;
-    add_dnodeint_end(&head, 0);
     add_dnodeint_end(&head, 1);
-    add_dnodeint_end(&head, 2);
-    add_dnodeint_end(&head, 3);
-    add_dnodeint_end(&head, 4);
-    add_dnodeint_end(&head, 98);
-    add_dnodeint_end(&head, 402);
-    add_dnodeint_end(&head, 1024);
     print_dlistint(head);
     printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 5);
+    delete_dnodeint_at_index(&head, 0);
     print_dlistint(head);
     printf("-----------------\n");
     delete_dnodeint_at_index(&head, 0);
---
+++ 0x19-hash_tables/.#0-hash_table_create.c
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.22206:1557274900
