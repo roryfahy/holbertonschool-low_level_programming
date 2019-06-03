b4279b7d41421ffe518c5487093c11f902314b8f
rmf10003 <685@holbertonschool.com>
2019-06-03 18:55:18 +0000

    delete hash table

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -1,11 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete -
- *
- * Return: 
+ * hash_table_delete - delete a hash table
+ * @ht: hash table
+ * Return: void
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i = 0;
+	hash_node_t *ptrA, *ptrB;
 
+	if (ht != NULL)
+	{
+		ptrA = ptrB = NULL;
+		for (; i < ht->size; i++)
+			if (ht->array[i] != NULL)
+			{
+				ptrA = ht->array[i];
+				while (ptrA != NULL)
+				{
+					ptrB = ptrA->next;
+					free(ptrA->key), free(ptrA->value), free(ptrA);
+					ptrA = ptrB;
+				}
+			}
+		free(ht->array);
+		free(ht);
+	}
 }
