5ce9232e7813b11bd42bf87225044bbb47f536a4
rmf10003 <685@holbertonschool.com>
2019-06-03 18:10:13 +0000

    check for ht being NULL

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *node_ptr, *tmp_node = NULL;
 
-	if (key == NULL || value == NULL || *key == '\0')
+	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
 		return (0);
 	node_ptr = calloc(1, sizeof(hash_node_t));
 	if (node_ptr == NULL)
