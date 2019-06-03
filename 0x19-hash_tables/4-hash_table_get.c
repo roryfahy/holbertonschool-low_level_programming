dabe505b3dbedb7bfbdb26c0616c0f3f6b5a3894
rmf10003 <685@holbertonschool.com>
2019-06-03 18:17:33 +0000

    add null check for key, *key and ht

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -11,6 +11,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *node;
 	unsigned long int index;
 
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 	index = key_index((unsigned char *) key, ht->size);
 	if (ht->array[index] == NULL)
 		return (NULL);
