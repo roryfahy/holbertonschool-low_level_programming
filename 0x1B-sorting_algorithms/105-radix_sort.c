64b50f288a0cae25aae2bfe931882a999df67583
rmf10003 <685@holbertonschool.com>
2019-06-17 20:39:54 +0000

    Cut memory usage in radix sort

--- 0x1B-sorting_algorithms/105-radix_sort.c
+++ 0x1B-sorting_algorithms/105-radix_sort.c
@@ -1,46 +1,42 @@
+#include <stdint.h>
 #include "sort.h"
 
 /**
- * radix_sort -
- *
- * Return: 
+ * radix_sort - sort with radix sort, heavily favoring speed over memory usage
+ * @array: array to sort
+ * @size: size of array
  */
 void radix_sort(int *array, size_t size)
 {
 	unsigned char digit;
-	size_t *buckets, *counts, index, total;
-	int pow10 = 1;
+	size_t counts[10];
+	size_t index, total;
+	int *copy, pow10 = 1;
 
 	if (array == NULL || size < 2)
 		return;
-	counts = malloc(size * sizeof(size_t) + size * 10 * sizeof(size_t));
-	if (counts == NULL)
+	copy = malloc(size * sizeof(int));
+	if (copy == NULL)
 		return;
-	buckets = counts + size;
 	do {
 		total = 0;
 		for (digit = 0; digit < 10; digit++)
 			counts[digit] = 0;
-		for (digit = 0; digit < 10; digit++)
+		for (index = 0; index < size; index++)
 		{
-			for (index = 0; index < size; index++)
-			{
-				if (array[index] / pow10 % 10 == digit)
-				{
-					buckets[size * digit + counts[digit]++] = array[index];
-					total += digit == 0 ? 0 : 1;
-				}
-			}
+			digit = array[index] / pow10 % 10;
+			counts[digit]++;
+			total += digit % 10 == 0 ? 0 : 1;
 		}
 		if (total == 0)
 			continue;
-		for (digit = total = 0; digit < 10; digit++)
-		{
-			for (index = 0; index < counts[digit]; index++, total++)
-				array[total] = buckets[size * digit + index];
-		}
+		for (digit = 1; digit < 10; digit++)
+			counts[digit] += counts[digit - 1];
+		for (index = size - 1; index != SIZE_MAX; index--)
+			copy[counts[array[index] / pow10 % 10]-- - 1] = array[index];
+		for (index = 0; index < size; index++)
+			array[index] = copy[index];
 		pow10 *= 10;
 		print_array(array, size);
 	} while (total != 0 && pow10 <= 1000000000);
-	free(counts);
 }
