f18cbf0cc38f9ef20b0f7dd6a01cc1e176a66506
rmf10003 <685@holbertonschool.com>
2019-07-24 13:47:19 +0000

    [global][c] returns ptr to uncle node

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -1,11 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_uncle -
- *
- * Return: 
+ * binary_tree_uncle - find the uncle of a node
+ * @node: ptr to node
+ * Return: ptr to uncle node
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	binary_tree_t *ma, *grandma;
 
+	ma = node->parent;
+	grandma = ma->parent;
+	if (node == NULL || ma == NULL || grandma == NULL)
+		return (NULL);
+	if (grandma->left != ma)
+		return (grandma->left);
+	return (grandma->right);
 }
