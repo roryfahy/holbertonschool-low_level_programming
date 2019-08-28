af40d406a08aae6a8cd86f300574da6387793953
rmf10003 <685@holbertonschool.com>
2019-08-28 18:17:45 +0000

    check if value is less that first value in array

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,22 +11,22 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int idx, iCheck, iStop = (int) size - 1;
-	int iStart = 0;
+	size_t idx, iCheck, iStop = size - 1;
+	size_t iStart = 0;
 
-	if (array == NULL)
+	if (array == NULL || array[0] > value)
 		return (-1);
 
 	iCheck = iStop / 2;
 	while (1)
 	{
+		if (iStop < iStart)
+			return (-1);
 		printf("Searching in array: ");
 		for (idx = iStart; idx <= iStop; idx++)
 			printf(idx < iStop ? "%d, " : "%d\n", array[idx]);
-		if (iStop < iStart)
-			return (-1);
 		if (array[iCheck] == value)
-			return (iCheck);
+			return ((int) iCheck);
 		if (array[iCheck] < value)
 		{
 			iStart = iCheck + 1;
